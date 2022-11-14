fbb76de7dfb852c81cc7ad8838d91d9bf0ca4b79
Aurora <jakersaf@gmail.com>
2022-11-14 01:40:10 -0300

    Make things more pleasing for future development
    Added comments on javascript to not forget how to do some
    things tomorrow on the project, and make things centralized.
    The frontend is kinda more pleasing this way i think idk

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const container = document.querySelector('.container')
 
+// create 16x16 grid layout
+// 256 is 16x16 just so you know
+
+// to make "choosable"
+// button input get a number
+// multiply number by self
+// just put result on for loop
+// reuse loop changing values
 for (let i = 1; i < 257; i++) {
   let square = document.createElement('div')
   square.className = 'square'
-  square.innerText = `${i}`
+  // square.innerText = `${i}`
   container.appendChild(square)
   square.addEventListener('mouseenter', function (e) {
     e.target.style.background = '#e3e6eb'
