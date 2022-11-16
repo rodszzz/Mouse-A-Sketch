c40fe86588407185853d69d505ecb6d94137327a
Aurora <jakersaf@gmail.com>
2022-11-16 19:58:55 -0300

    "Finish" project and add nice button
    THAT WAS SUPER FUN OMG I learned a lot through it and i think
    I'll revisit it later to add some more functionalities and a bit
    more of style, cuz it's really ugly rn lol

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const container = document.querySelector('.container')
+// const button = document.querySelector('.button')
 
-// create 16x16 grid layout
-// 256 is 16x16 just so you know
+// let x = prompt('Input number between 1 and 100, please')
+// console.log(x)
+const x = 16
+container.style.setProperty('--numColumns', x)
 
-// to make "choosable"
-// button input get a number
-// multiply number by self
-// just put result on for loop
-// reuse loop changing values
-for (let i = 1; i < 257; i++) {
+for (let i = 0; i < x * x; i++) {
   let square = document.createElement('div')
   square.className = 'square'
   // square.innerText = `${i}`
@@ -18,3 +16,51 @@ for (let i = 1; i < 257; i++) {
     e.target.style.color = '#e3e6eb'
   })
 }
+
+document.getElementById('button').onclick = function () {
+  // this removes all the squares from before to create new ones
+  let squareRemove = container.firstElementChild
+  while (squareRemove) {
+    squareRemove.remove()
+    squareRemove = container.firstElementChild
+  }
+
+  let x = prompt('Input number between 1 and 100, please')
+  // console.log(x)
+
+  container.style.setProperty('--numColumns', x)
+  // container.style.setProperty('--numRows', x)
+
+  for (let i = 0; i < x * x; i++) {
+    let square = document.createElement('div')
+    square.className = 'square'
+    // square.innerText = `${i}`
+    container.appendChild(square)
+    square.addEventListener('mouseenter', function (e) {
+      e.target.style.background = '#e3e6eb'
+      e.target.style.color = '#e3e6eb'
+    })
+  }
+}
+
+// console.log(x)
+
+// create 16x16 grid layout
+// 256 is 16x16 just so you know
+
+// to make "choosable"
+// button input get a number
+// multiply number by self
+// just put result on for loop
+// reuse loop changing values
+
+// for (let i = 1; i < 257; i++) {
+//   let square = document.createElement('div')
+//   square.className = 'square'
+//   // square.innerText = `${i}`
+//   container.appendChild(square)
+//   square.addEventListener('mouseenter', function (e) {
+//     e.target.style.background = '#e3e6eb'
+//     e.target.style.color = '#e3e6eb'
+//   })
+// }
